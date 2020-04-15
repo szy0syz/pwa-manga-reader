8ddee70ba3c6ad70ae27a91edac52bf5f55931ca
szy0syz <szy0syz@gmail.com>
2020-04-15 10:22:04 +0800

    Add PWA base

--- reader-app/src/index.jsx
+++ reader-app/src/index.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 
-render(<h1>Working</h1>, document.getElementById('app'));
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("service-worker.js");
+}
+
+render(<h1>Working~</h1>, document.getElementById("app"));
